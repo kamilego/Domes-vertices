80ccb342f83c919939be9456f9796ab6ffb36179
kamilego <kam.legowicz@gmail.com>
2021-05-24 23:36:40 +0200

    tryign to figure out how to count division ratio value for points

--- vertices.py
+++ vertices.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import numpy as np
+import math
+
+
+def vert_rota(degree):
+    a1 = math.cos(math.radians(degree))
+    a2 = math.sin(math.radians(degree))
+    return a1, a2
+
+
+def hor_vert_ratio(degree, dif, a):
+    if dif > 0:
+        return vert_rota(degree+dif)[a]/vert_rota(degree)[a]
+    else:
+        return vert_rota(degree)[a]/vert_rota(degree+dif)[a]
 
 path = r"D:\Programy\z.studi\ROK 6\magister\vertices_new\test\vertices.xlsx"
 
@@ -10,7 +24,9 @@ ver_list = df.to_numpy()
 ver_list = ver_list[ver_list[:,2].argsort()]
 
 dict_split = {}
+degrees_list = np.arange(0, 91, 11.25)
 
+num = 0
 for i in range(0, len(ver_list), 20):
-    dict_split["list_"+str(i)] = ver_list[i:20+i]
-
+    dict_split["list_"+str(num)] = ver_list[i:20+i]
+    num += 11.25
